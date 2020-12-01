666fb81ae90df97968d4f793e42aa79940ecdb2e
Mikael Arthur <mikael.atala@gmail.com>
2020-12-01 17:33:44 -0300

    Start Neele project

---
+++ src/pages/index.js
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="#">Neele!</a>
+        </h1>
+
+        
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Develop by{' '}
+          <h3>Uabit</h3>
+        </a>
+      </footer>
+    </div>
+  )
+}
