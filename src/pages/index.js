5ebf3acbd83e1e133034d9c611db11b86b0f3bbd
Mikael Arthur <mikael.atala@gmail.com>
2020-12-02 11:37:57 -0300

    implementacao das imagens e correcao do css

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,30 +3,41 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Neele</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"/>
+        <link rel="preload"
+            href="/fonts/SFPro/SFPro-Semibold.ttf"
+            as="font"
+            crossOrigin=""/>
+        <link rel="preload"
+            href="/fonts/SFPro/SFPro-Regular.ttf"
+            as="font"
+            crossOrigin=""/>
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="#">Neele!</a>
-        </h1>
-
-        
+      <main>
+        <div className="container">
+          <div className="row pt-4">
+            <div className="col ml-5">
+              <div className="d-flex pb-2">
+                <img className="logo-neele" src="img/Neele-app.png" alt=""/>
+                <h1 className="title-neele mb-0 align-self-end">eele</h1>
+              </div>
+              <h2 className="text-white py-3">Learn a language faster by speaking it</h2>
+              <p className="text-dark-green py-3">unique method based on effective techniques used by famous Pollygots.</p>
+                <div className="row justify-content-around py-5">
+                  <img className="icone-apple" src="img/apple-icon2.png" alt=""/>
+                  <img className="icone-goole" src="img/google-icon2.png" alt=""/>
+                </div>
+            </div>
+            <div className="col">
+              <img className="img-section" src="img/Pasted-7.png" alt=""/>
+            </div>
+          </div>
+        </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Develop by{' '}
-          <h3>Uabit</h3>
-        </a>
-      </footer>
-    </div>
+    </>
   )
 }
